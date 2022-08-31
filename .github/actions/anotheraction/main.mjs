a3767f68db55678feacecd0a382849156d06530e
Johannes Normann Jensen <johannes@nestech.dk>
2022-08-31 17:35:14 +0200

    Update main.mjs

--- .github/actions/anotheraction/main.mjs
+++ .github/actions/anotheraction/main.mjs
@@ -1,5 +1,7 @@
 import { things } from '../utils.mjs';
+import * as core from '@actions/core';
 
 console.log('hello');
+core.info('hej fra info log')
 console.log('heres a list of things', things.join(', '));
 console.log('the end');
